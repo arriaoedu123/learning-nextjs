53c514b5d3684b6db2b5b1c08d4f365377cf0935
Aryon Rabello <73148019+arriaoedu123@users.noreply.github.com>
2022-07-25 20:58:27 +0000

    first commit

---
+++ 1_hello-world/pages/about.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+      <h1>This is the about page</h1>
+    </div>
+  );
+}
